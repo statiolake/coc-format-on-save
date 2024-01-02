e80c7ded5ede63767644a615e30e8db1e8d3641e
statiolake <statiolake@gmail.com>
2024-01-03 04:28:14 +0900

    fix: check organizeImports exists before running

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-import { Document, ExtensionContext, WorkspaceConfiguration, commands, languages, window, workspace } from 'coc.nvim';
+import {
+  CancellationTokenSource,
+  Document,
+  ExtensionContext,
+  Range,
+  WorkspaceConfiguration,
+  commands,
+  languages,
+  window,
+  workspace,
+} from 'coc.nvim';
+
+const channel = window.createOutputChannel('format-on-save');
 
 export async function activate(context: ExtensionContext): Promise<void> {
   const config = getConfig();
@@ -22,6 +34,20 @@ function isCocConfigFile(doc: Document): boolean {
   return doc.uri.endsWith('coc-settings.json');
 }
 
+async function hasOrganizeImport(doc: Document): Promise<boolean> {
+  const range = Range.create(0, 0, doc.lineCount, 0);
+  const context = {
+    diagnostics: [],
+    only: ['source.organizeImports' /* CodeActionKind.SourceOrganizeImports */],
+    triggerKind: 1 /* CodeActionTriggerKind.Invoked */,
+  };
+  const tokenSource = new CancellationTokenSource();
+  // @ts-ignore
+  const codeActions = await languages.getCodeActions(doc.textDocument, range, context, tokenSource.token);
+  channel.appendLine(`codeActions: ${JSON.stringify(codeActions)}`);
+  return codeActions && codeActions.length;
+}
+
 async function format() {
   const config = getConfig();
   if (!config.get<boolean>('formatOnSave')) return;
@@ -31,30 +57,38 @@ async function format() {
     try {
       await commands.executeCommand('formatJson', '--sort-keys');
     } catch (e) {
-      window.showWarningMessage('Failed to sort coc-settings.json');
+      void window.showWarningMessage(`Failed to sort coc-settings.json: ${e}`);
     }
   }
 
   // @ts-ignore
   if (!languages.hasFormatProvider(doc.textDocument)) {
-    window.showWarningMessage('Format provider not found for current document');
+    void window.showWarningMessage('Format provider not found for current document');
     return;
   }
 
   try {
     if (config.get<boolean>('organizeImportOnFormat')) {
-      await commands.executeCommand('editor.action.organizeImport');
+      if (await hasOrganizeImport(doc)) {
+        channel.appendLine('organize imports');
+        await commands.executeCommand('editor.action.organizeImport');
+      } else {
+        channel.appendLine('organizeImport is not supported');
+      }
     }
+    channel.appendLine('format document');
     await commands.executeCommand('editor.action.formatDocument');
   } catch (e) {
-    window.showErrorMessage('Failed to format document');
+    void window.showErrorMessage(`Failed to format document: ${e}`);
   }
 }
 
 async function bufWritePreCallback() {
-  const buffer = await workspace.nvim.buffer;
-  const skip_once = await buffer.getVar('coc_format_on_save_skip_once');
-  await buffer.setVar('coc_format_on_save_skip_once', false);
+  const doc = await workspace.document;
+  doc.forceSync();
+  const skip_once = await doc.buffer.getVar('coc_format_on_save_skip_once');
+  await doc.buffer.setVar('coc_format_on_save_skip_once', false);
   if (skip_once) return;
   await format();
+  doc.forceSync();
 }
